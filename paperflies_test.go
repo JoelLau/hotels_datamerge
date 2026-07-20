4591d3dc452bd3e96d7821a39ad01bffadf92f28
Joel Lau <joellau@protonmail.com>
2026-07-20 17:45:55 +0800

    reorg test files

--- paperflies_test.go
+++ paperflies_test.go
@@ -9,10 +9,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestPaperfliesHotelUnmarshal(t *testing.T) {
+func TestPaperfliesHotel(t *testing.T) {
 	t.Parallel()
 
-	sampleJSON := `[
+	t.Run("Unmarshal", func(t *testing.T) {
+		t.Parallel()
+
+		sampleJSON := `[
   {
     "hotel_id": "iJhz",
     "destination_id": 5432,
@@ -94,100 +97,101 @@ func TestPaperfliesHotelUnmarshal(t *testing.T) {
   }
 ]`
 
-	var got []hotels.PaperfliesHotel
-	require.NoError(t, json.Unmarshal([]byte(sampleJSON), &got))
-	require.Len(t, got, 3)
+		var got []hotels.PaperfliesHotel
+		require.NoError(t, json.Unmarshal([]byte(sampleJSON), &got))
+		require.Len(t, got, 3)
 
-	want := []hotels.PaperfliesHotel{
-		{
-			HotelID:       "iJhz",
-			DestinationID: 5432,
-			HotelName:     "Beach Villas Singapore",
-			Location: hotels.PaperfliesLocation{
-				Address: "8 Sentosa Gateway, Beach Villas, 098269",
-				Country: "Singapore",
-			},
-			Details: "Surrounded by tropical gardens, these upscale villas in elegant Colonial-style buildings are part of the Resorts World Sentosa complex.",
-			Amenities: hotels.PaperfliesAmenities{
-				General: []string{"outdoor pool", "indoor pool", "business center", "childcare"},
-				Room:    []string{"tv", "coffee machine", "kettle", "hair dryer", "iron"},
-			},
-			Images: hotels.PaperfliesImages{
-				Rooms: []hotels.PaperfliesImage{
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/2.jpg", Caption: "Double room"},
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/3.jpg", Caption: "Double room"},
+		want := []hotels.PaperfliesHotel{
+			{
+				HotelID:       "iJhz",
+				DestinationID: 5432,
+				HotelName:     "Beach Villas Singapore",
+				Location: hotels.PaperfliesLocation{
+					Address: "8 Sentosa Gateway, Beach Villas, 098269",
+					Country: "Singapore",
 				},
-				Site: []hotels.PaperfliesImage{
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/1.jpg", Caption: "Front"},
+				Details: "Surrounded by tropical gardens, these upscale villas in elegant Colonial-style buildings are part of the Resorts World Sentosa complex.",
+				Amenities: hotels.PaperfliesAmenities{
+					General: []string{"outdoor pool", "indoor pool", "business center", "childcare"},
+					Room:    []string{"tv", "coffee machine", "kettle", "hair dryer", "iron"},
 				},
-			},
-			BookingConditions: []string{
-				"All children are welcome. One child under 12 years stays free of charge when using existing beds.",
-				"Pets are not allowed.",
-				"WiFi is available in all areas and is free of charge.",
-				"Free private parking is possible on site (reservation is not needed).",
-				"Guests are required to show a photo identification and credit card upon check-in.",
-			},
-		},
-		{
-			HotelID:       "SjyX",
-			DestinationID: 5432,
-			HotelName:     "InterContinental",
-			Location: hotels.PaperfliesLocation{
-				Address: "1 Nanson Rd, Singapore 238909",
-				Country: "Singapore",
-			},
-			Details: "InterContinental Singapore Robertson Quay is luxury's preferred address offering stylishly cosmopolitan riverside living for discerning travelers to Singapore.",
-			Amenities: hotels.PaperfliesAmenities{
-				General: []string{"outdoor pool", "business center", "childcare", "parking", "bar", "dry cleaning", "wifi", "breakfast", "concierge"},
-				Room:    []string{"aircon", "minibar", "tv", "bathtub", "hair dryer"},
-			},
-			Images: hotels.PaperfliesImages{
-				Rooms: []hotels.PaperfliesImage{
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i93_m.jpg", Caption: "Double room"},
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i94_m.jpg", Caption: "Bathroom"},
+				Images: hotels.PaperfliesImages{
+					Rooms: []hotels.PaperfliesImage{
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/2.jpg", Caption: "Double room"},
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/3.jpg", Caption: "Double room"},
+					},
+					Site: []hotels.PaperfliesImage{
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/1.jpg", Caption: "Front"},
+					},
 				},
-				Site: []hotels.PaperfliesImage{
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i1_m.jpg", Caption: "Restaurant"},
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i2_m.jpg", Caption: "Hotel Exterior"},
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i5_m.jpg", Caption: "Entrance"},
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i24_m.jpg", Caption: "Bar"},
+				BookingConditions: []string{
+					"All children are welcome. One child under 12 years stays free of charge when using existing beds.",
+					"Pets are not allowed.",
+					"WiFi is available in all areas and is free of charge.",
+					"Free private parking is possible on site (reservation is not needed).",
+					"Guests are required to show a photo identification and credit card upon check-in.",
 				},
 			},
-			BookingConditions: []string{},
-		},
-		{
-			HotelID:       "f8c9",
-			DestinationID: 1122,
-			HotelName:     "Hilton Tokyo",
-			Location: hotels.PaperfliesLocation{
-				Address: "160-0023, SHINJUKU-KU, 6-6-2 NISHI-SHINJUKU, JAPAN",
-				Country: "Japan",
-			},
-			Details: "This sleek high-rise property is 10 minutes' walk from Shinjuku train station, 6 minutes' walk from the Tokyo Metropolitan Government Building and 3 km from Yoyogi Park.",
-			Amenities: hotels.PaperfliesAmenities{
-				General: []string{"indoor pool", "business center", "wifi"},
-				Room:    []string{"tv", "aircon", "minibar", "bathtub", "hair dryer"},
-			},
-			Images: hotels.PaperfliesImages{
-				Rooms: []hotels.PaperfliesImage{
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i1_m.jpg", Caption: "Suite"},
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i15_m.jpg", Caption: "Double room"},
+			{
+				HotelID:       "SjyX",
+				DestinationID: 5432,
+				HotelName:     "InterContinental",
+				Location: hotels.PaperfliesLocation{
+					Address: "1 Nanson Rd, Singapore 238909",
+					Country: "Singapore",
 				},
-				Site: []hotels.PaperfliesImage{
-					{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i55_m.jpg", Caption: "Bar"},
+				Details: "InterContinental Singapore Robertson Quay is luxury's preferred address offering stylishly cosmopolitan riverside living for discerning travelers to Singapore.",
+				Amenities: hotels.PaperfliesAmenities{
+					General: []string{"outdoor pool", "business center", "childcare", "parking", "bar", "dry cleaning", "wifi", "breakfast", "concierge"},
+					Room:    []string{"aircon", "minibar", "tv", "bathtub", "hair dryer"},
+				},
+				Images: hotels.PaperfliesImages{
+					Rooms: []hotels.PaperfliesImage{
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i93_m.jpg", Caption: "Double room"},
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i94_m.jpg", Caption: "Bathroom"},
+					},
+					Site: []hotels.PaperfliesImage{
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i1_m.jpg", Caption: "Restaurant"},
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i2_m.jpg", Caption: "Hotel Exterior"},
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i5_m.jpg", Caption: "Entrance"},
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/Sjym/i24_m.jpg", Caption: "Bar"},
+					},
+				},
+				BookingConditions: []string{},
+			},
+			{
+				HotelID:       "f8c9",
+				DestinationID: 1122,
+				HotelName:     "Hilton Tokyo",
+				Location: hotels.PaperfliesLocation{
+					Address: "160-0023, SHINJUKU-KU, 6-6-2 NISHI-SHINJUKU, JAPAN",
+					Country: "Japan",
+				},
+				Details: "This sleek high-rise property is 10 minutes' walk from Shinjuku train station, 6 minutes' walk from the Tokyo Metropolitan Government Building and 3 km from Yoyogi Park.",
+				Amenities: hotels.PaperfliesAmenities{
+					General: []string{"indoor pool", "business center", "wifi"},
+					Room:    []string{"tv", "aircon", "minibar", "bathtub", "hair dryer"},
+				},
+				Images: hotels.PaperfliesImages{
+					Rooms: []hotels.PaperfliesImage{
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i1_m.jpg", Caption: "Suite"},
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i15_m.jpg", Caption: "Double room"},
+					},
+					Site: []hotels.PaperfliesImage{
+						{Link: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i55_m.jpg", Caption: "Bar"},
+					},
+				},
+				BookingConditions: []string{
+					"All children are welcome. One child under 6 years stays free of charge when using existing beds. There is no capacity for extra beds in the room.",
+					"Pets are not allowed.",
+					"Wired internet is available in the hotel rooms and charges are applicable. WiFi is available in the hotel rooms and charges are applicable.",
+					"Private parking is possible on site (reservation is not needed) and costs JPY 1500 per day.",
+					"When booking more than 9 rooms, different policies and additional supplements may apply.",
+					"The hotel's free shuttle is offered from Bus Stop #21 in front of Keio Department Store at Shinjuku Station.",
 				},
 			},
-			BookingConditions: []string{
-				"All children are welcome. One child under 6 years stays free of charge when using existing beds. There is no capacity for extra beds in the room.",
-				"Pets are not allowed.",
-				"Wired internet is available in the hotel rooms and charges are applicable. WiFi is available in the hotel rooms and charges are applicable.",
-				"Private parking is possible on site (reservation is not needed) and costs JPY 1500 per day.",
-				"When booking more than 9 rooms, different policies and additional supplements may apply.",
-				"The hotel's free shuttle is offered from Bus Stop #21 in front of Keio Department Store at Shinjuku Station.",
-			},
-		},
-	}
+		}
 
-	require.Equal(t, want, got)
+		require.Equal(t, want, got)
+	})
 }
--- patagonia_test.go
+++ patagonia_test.go
@@ -9,10 +9,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestPatagoniaHotelUnmarshal(t *testing.T) {
+func TestPatagoniaHotel(t *testing.T) {
 	t.Parallel()
 
-	sampleJSON := `[
+	t.Run("Unmarshal", func(t *testing.T) {
+		t.Parallel()
+
+		sampleJSON := `[
   {
     "id": "iJhz",
     "destination": 5432,
@@ -54,51 +57,52 @@ func TestPatagoniaHotelUnmarshal(t *testing.T) {
   }
 ]`
 
-	var got []hotels.PatagoniaHotel
-	require.NoError(t, json.Unmarshal([]byte(sampleJSON), &got))
-	require.Len(t, got, 2)
+		var got []hotels.PatagoniaHotel
+		require.NoError(t, json.Unmarshal([]byte(sampleJSON), &got))
+		require.Len(t, got, 2)
 
-	want := []hotels.PatagoniaHotel{
-		{
-			ID:          "iJhz",
-			Destination: 5432,
-			Name:        "Beach Villas Singapore",
-			Latitude:    1.264751,
-			Longitude:   103.824006,
-			Address:     new("8 Sentosa Gateway, Beach Villas, 098269"),
-			Info:        new("Located at the western tip of Resorts World Sentosa, guests at the Beach Villas are guaranteed privacy while they enjoy spectacular views of glittering waters."),
-			Amenities:   []string{"Aircon", "Tv", "Coffee machine", "Kettle", "Hair dryer", "Iron", "Tub"},
-			Images: hotels.PatagoniaImages{
-				Rooms: []hotels.PatagoniaImage{
-					{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/2.jpg", Description: "Double room"},
-					{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/4.jpg", Description: "Bathroom"},
-				},
-				Amenities: []hotels.PatagoniaImage{
-					{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/0.jpg", Description: "RWS"},
-					{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/6.jpg", Description: "Sentosa Gateway"},
+		want := []hotels.PatagoniaHotel{
+			{
+				ID:          "iJhz",
+				Destination: 5432,
+				Name:        "Beach Villas Singapore",
+				Latitude:    1.264751,
+				Longitude:   103.824006,
+				Address:     new("8 Sentosa Gateway, Beach Villas, 098269"),
+				Info:        new("Located at the western tip of Resorts World Sentosa, guests at the Beach Villas are guaranteed privacy while they enjoy spectacular views of glittering waters."),
+				Amenities:   []string{"Aircon", "Tv", "Coffee machine", "Kettle", "Hair dryer", "Iron", "Tub"},
+				Images: hotels.PatagoniaImages{
+					Rooms: []hotels.PatagoniaImage{
+						{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/2.jpg", Description: "Double room"},
+						{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/4.jpg", Description: "Bathroom"},
+					},
+					Amenities: []hotels.PatagoniaImage{
+						{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/0.jpg", Description: "RWS"},
+						{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/0qZF/6.jpg", Description: "Sentosa Gateway"},
+					},
 				},
 			},
-		},
-		{
-			ID:          "f8c9",
-			Destination: 1122,
-			Name:        "Hilton Tokyo Shinjuku",
-			Latitude:    35.6926,
-			Longitude:   139.690965,
-			Address:     nil,
-			Info:        nil,
-			Amenities:   nil,
-			Images: hotels.PatagoniaImages{
-				Rooms: []hotels.PatagoniaImage{
-					{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i10_m.jpg", Description: "Suite"},
-					{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i11_m.jpg", Description: "Suite - Living room"},
-				},
-				Amenities: []hotels.PatagoniaImage{
-					{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i57_m.jpg", Description: "Bar"},
+			{
+				ID:          "f8c9",
+				Destination: 1122,
+				Name:        "Hilton Tokyo Shinjuku",
+				Latitude:    35.6926,
+				Longitude:   139.690965,
+				Address:     nil,
+				Info:        nil,
+				Amenities:   nil,
+				Images: hotels.PatagoniaImages{
+					Rooms: []hotels.PatagoniaImage{
+						{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i10_m.jpg", Description: "Suite"},
+						{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i11_m.jpg", Description: "Suite - Living room"},
+					},
+					Amenities: []hotels.PatagoniaImage{
+						{URL: "https://d2ey9sqrvkqdfs.cloudfront.net/YwAr/i57_m.jpg", Description: "Bar"},
+					},
 				},
 			},
-		},
-	}
+		}
 
-	require.Equal(t, want, got)
+		require.Equal(t, want, got)
+	})
 }
